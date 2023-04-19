1324ee2c75feccd482a4da894105e182ddaa6124
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2023-04-19 08:44:45 -0500

    Various changes from a while ago that've piled up
    * Stop Darkreader entension from double-darkening my page
    * Add some services I use (Mastodon, Reddit, last.fm, MusicBrainz, more
      maybe)
    * Fix layout, broke it again maybe
    * some silly new splashes!!1!

--- splash.js
+++ splash.js
@@ -1,19 +1,26 @@
 try {
     $.get("/splashes.txt", function(data) {
         const splashes = data.split("\n");
-        const randomSplashIndex = Math.floor(Math.random() * splashes.length)/* * 0 + 5-1 */;
-        var splash = splashes[randomSplashIndex];
+        var randomSplashIndex = Math.floor(Math.random() * splashes.length);
+        
+        /*
+        var randomSplashIndex = 5;
+        */
+        
+        var splash = splashes[randomSplashIndex - 1]; // zero indexing?? very funny...
         
         if (splash.includes("{{ip}}")) {
             splash = splash.replace("{{ip}}", `${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}`);
         }
         
-        $('#splash').text(splash);
+        $('.splash').text(splash);
     }).fail(function() {
-        document.getElementById("splash").textContent = "missingno";
-        console.error("Splashes failed to load!!1! (001)");
+        $('.splash').textContent = "missingno";
+        console.error("Splashes failed to load!!1! (001 : Failed to get splashes file)");
+        console.error(e);
     });
 } catch (e) {
-    document.getElementById("splash").textContent = "missingno";
-    console.error("Splashes failed to load!!1! (002)");
+    $('.splash').textContent = "missingno";
+    console.error("Splashes failed to load!!1! (002 : Something has gone terribly wrong)");
+    console.error(e);
 }
