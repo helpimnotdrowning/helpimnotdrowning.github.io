f4a2c1b3e5376bcaea27fde6e62882fd70bd9a1a
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2023-08-23 22:52:05 -0500

    Add socials, BG blur, new splashes + splash fixes

--- splash.js
+++ splash.js
@@ -1,26 +1,31 @@
-try {
-    $.get("/splashes.txt", function(data) {
-        const splashes = data.split("\n");
-        var randomSplashIndex = Math.floor(Math.random() * splashes.length);
-        
-        /*
-        var randomSplashIndex = 5;
-        */
-        
-        var splash = splashes[randomSplashIndex - 1]; // zero indexing?? very funny...
-        
-        if (splash.includes("{{ip}}")) {
-            splash = splash.replace("{{ip}}", `${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}`);
-        }
-        
-        $('.splash').text(splash);
-    }).fail(function() {
-        $('.splash').textContent = "missingno";
-        console.error("Splashes failed to load!!1! (001 : Failed to get splashes file)");
-        console.error(e);
-    });
-} catch (e) {
-    $('.splash').textContent = "missingno";
-    console.error("Splashes failed to load!!1! (002 : Something has gone terribly wrong)");
-    console.error(e);
+function GetSplash(callback) {
+    try {
+        // I should probably not be retrieving the entire file every time this is called,
+        // but the browser caches the file so ¯\_(ツ)_/¯
+        $.get("/splashes.txt", function(data) {
+            const splashes = data.split("\n");
+            
+            var i = Math.floor(Math.random() * (splashes.length));
+            var splash = splashes[i]
+            
+            if (splash.includes("{{ip}}")) {
+                splash = splash.replace("{{ip}}", `${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}.${Math.round(Math.random()*255)}`);
+            }
+            
+            callback(splash);
+            
+        }).fail(function() {
+            console.error("Splashes failed to load!!1! (001 : Failed to get splashes file)");
+            callback("missingno");
+        });
+    }
+    catch (e) {
+        console.error("Splashes failed to load!!1! (002 : Something has gone terribly wrong)");
+        console.error(e.message);
+        callback("missingno");
+    }
 }
+
+function SetSplash() {
+    GetSplash( (splash) => $('.splash').text(splash) );
+  }
